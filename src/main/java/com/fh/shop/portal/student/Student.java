aaaa854ed7df32c4cfc8b34e5722fbfb8e836c44
liyihan <1638401515@qq.com>
2020-08-11 15:03:50 +0800

    init pro

---
+++ src/main/java/com/fh/shop/portal/student/Student.java
@@ -0,0 +1,11 @@
+package com.fh.shop.portal.student;
+
+public class Student {
+
+
+    public static void main(String[] args){
+
+        System.out.println("==================");
+        System.out.println("韩阳阳");
+    }
+}
